9a5b999428af49a887c82a3e30bfd109ecdece68
DarioGiuliani <32555159+DarioGiuliani@users.noreply.github.com>
2023-12-03 17:37:06 +0000

    Add solution day 2 part 1

--- AoC/solutions/classes/game.py
+++ AoC/solutions/classes/game.py
@@ -3,8 +3,19 @@ class Game:
         self.id = id
         self.cubes = cubes
 
+    def is_possible(self, red, blue, green):
+        for subset in self.cubes:
+            cubes = Cubes(subset.red, subset.blue, subset.green)
+            if not cubes.is_subset_possible(red, blue, green):
+                return False
+        return True
+
+
 class Cubes:
     def __init__(self, red, blue, green):
         self.red = red
         self.blue = blue
         self.green = green
+
+    def is_subset_possible(self, red, blue, green):
+        return self.red <= red and self.blue <= blue and self.green <= green
--- AoC/solutions/day2part1.py
+++ AoC/solutions/day2part1.py
@@ -2,3 +2,31 @@ from utils.file_reader import FileReader
 from classes.game import Game,Cubes
 
 input = FileReader.readfile("day2part1input.txt")
+total = 0
+
+for element in input:
+    stripped_element = element.strip()
+    split_game = stripped_element.split(":")
+    game_id = int(split_game[0].split(" ")[1].strip())
+    sub_sets = split_game[1].strip().split(";")
+    collect_subsets = []
+    for set in sub_sets:
+        set_split = set.strip().split(",")
+        green = 0
+        red = 0
+        blue = 0
+        for set_element in set_split:
+            if "green" in set_element:
+                green = int(set_element.strip().split(" ")[0].strip())
+            if "red" in set_element:
+                red = int(set_element.strip().split(" ")[0].strip())
+            if "blue" in set_element:
+                blue = int(set_element.strip().split(" ")[0].strip())
+        collect_subsets.append(Cubes(red, blue, green))
+    
+    game = Game(game_id, collect_subsets)
+    print(str.format("game_id: {}, is possible:{}", game.id, game.is_possible(12, 14, 13)))
+    if game.is_possible(12, 14, 13):  
+        total += game.id
+print(total)
+
