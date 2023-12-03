2b0ad171d421c2d86dc9a6d553db6d1edea0f11d
DarioGiuliani <32555159+DarioGiuliani@users.noreply.github.com>
2023-12-03 02:28:34 +0000

    Add setup day 2

---
+++ AoC/solutions/classes/game.py
@@ -0,0 +1,10 @@
+class Game:
+    def __init__(self, id, cubes):
+        self.id = id
+        self.cubes = cubes
+
+class Cubes:
+    def __init__(self, red, blue, green):
+        self.red = red
+        self.blue = blue
+        self.green = green
---
+++ AoC/solutions/day2part1.py
@@ -0,0 +1,4 @@
+from utils.file_reader import FileReader
+from classes.game import Game,Cubes
+
+input = FileReader.readfile("day2part1input.txt")
